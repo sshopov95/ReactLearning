67207cf7cc913d33416f0887c43b20ebf4a1ccef
Светослав Шопов <simobulgaria@gmail.com>
2026-02-06 10:25:42 +0200

    crypto build test

--- react-examples/vite/crypto-dash/vite.config.js
+++ react-examples/vite/crypto-dash/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    outDir: 'build', //Сменяме output директорията при билда - ако искаме (default - dist)
+    //minify: false, //Спира "мачкането" на кода в .js файла
+  }
 })
