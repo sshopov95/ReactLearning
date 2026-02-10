dc00729a9fec88aad9c1e104fab794a970b37f92
Светослав Шопов <simobulgaria@gmail.com>
2026-02-10 09:23:18 +0200

    markdown

--- react-examples/vite/friendly-dev/app/routes.ts
+++ react-examples/vite/friendly-dev/app/routes.ts
@@ -4,10 +4,11 @@ export default [
     //Добавяме страницата без да се импортва и да се пише <Route> - filebased route
     
     layout('./routes/layouts/home.tsx',[index("routes/home/index.tsx")]), //Специфичен Layout за Home
-    layout('./routes/layouts/main.tsx', [route('about', './routes/about/index.tsx'),
+    layout('./routes/layouts/main.tsx', [route('about', './routes/about/index.tsx'),  //Layout за всички останали пейджове
     route('contact', './routes/contact/index.tsx'),
     route('projects', './routes/projects/index.tsx'),
     route('projects/:id', './routes/projects/details.tsx'),//Специфичен раут за ID-тата
-    route('blog', './routes/blog/index.tsx')])  //Layout за всички останали пейджове
+    route('blog', './routes/blog/index.tsx'),
+    route('*', './routes/errors/not-found.tsx')]) //404 за всички останали
     
 ] satisfies RouteConfig;
