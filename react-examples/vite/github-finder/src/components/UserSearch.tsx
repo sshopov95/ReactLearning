e44a77a30ec7ecd6c90a38cafe60e8895f68107b
Светослав Шопов <simobulgaria@gmail.com>
2026-02-13 10:07:10 +0200

    user suggestions implemented

--- react-examples/vite/github-finder/src/components/UserSearch.tsx
+++ react-examples/vite/github-finder/src/components/UserSearch.tsx
@@ -1,20 +1,36 @@
 import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { fetchGithubUser } from "../api/github";
+import { fetchGithubUser, searchGithubUser } from "../api/github";
 import UserCard from "./UserCard";
 import RecentSearches from "./RecentSearches";
+import { useDebounce } from "use-debounce";
+import type { GithubUser } from "../types";
+import UserSuggestions, {
+  type SuggestionDropdownProps,
+} from "./UserSuggestions";
 
 const UserSearch = () => {
   const [username, setUsername] = useState("");
   const [submittedUserName, setSubmittedUsername] = useState("");
   const [recentUsers, setRecentUsers] = useState<string[]>(() => {
-    const stored = localStorage.getItem('recentUsers');
-    return stored ? JSON.parse(stored) : []; });
+    const stored = localStorage.getItem("recentUsers");
+    return stored ? JSON.parse(stored) : [];
+  });
+  const [debauncedUsername] = useDebounce(username, 300);
+  const [showDropdown, setShowDropdown] = useState(false);
 
-  const { data, isLoading, isError, error } = useQuery({
+  // Query to fetch specific user
+  const { data, isLoading, isError, error, refetch } = useQuery({
     queryKey: ["users", submittedUserName],
     queryFn: () => fetchGithubUser(submittedUserName),
     enabled: !!submittedUserName, //parse as bool
+  });
+
+  // Query to fetch suggestions for user search
+  const { data: suggestions } = useQuery({
+    queryKey: ["github-user-suggestions", debauncedUsername],
+    queryFn: () => searchGithubUser(debauncedUsername),
+    enabled: debauncedUsername.length > 1, //parse as bool
   });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -22,6 +38,7 @@ const UserSearch = () => {
     const trimmed = username.trim();
     if (!trimmed) return;
     setSubmittedUsername(trimmed);
+    setUsername("");
 
     setRecentUsers((prev) => {
       const updated = [trimmed, ...prev.filter((u) => u !== trimmed)]; //Filter duplicates
@@ -29,18 +46,45 @@ const UserSearch = () => {
     });
   };
 
-  useEffect(()=> {
-    localStorage.setItem('recentUsers',JSON.stringify(recentUsers));
-  },[recentUsers])
+  useEffect(() => {
+    localStorage.setItem("recentUsers", JSON.stringify(recentUsers));
+  }, [recentUsers]);
   return (
     <>
       <form className="form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter GitHub Username..."
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+        <div className="dropdown-wrapper">
+          <input
+            type="text"
+            placeholder="Enter GitHub Username..."
+            value={username}
+            onChange={(e) => {
+              const val = e.target.value;
+              setUsername(val);
+              setShowDropdown(val.trim().length > 1);
+            }}
+          />
+
+          {showDropdown && suggestions?.length > 0 && (
+            <UserSuggestions
+              show={showDropdown}
+              onSelect={(selected) => {
+                setUsername(selected);
+                setShowDropdown(false);
+
+                if (submittedUserName !== selected) {
+                  setSubmittedUsername(selected);
+                } else {
+                  refetch();
+                }
+                setRecentUsers((prev)=> {
+                     const updated = [selected, ...prev.filter((u) => u !== selected)]; //Filter duplicates
+                    return updated.slice(0, 5);
+                })
+              }}
+              suggestions={suggestions}
+            />
+          )}
+        </div>
         <button type="submit">Search</button>
       </form>
 
@@ -49,10 +93,13 @@ const UserSearch = () => {
 
       {data && <UserCard user={data} />}
       {recentUsers.length > 0 && (
-        <RecentSearches users={recentUsers} onSelect={(username)=>{
+        <RecentSearches
+          users={recentUsers}
+          onSelect={(username) => {
             setUsername(username);
             setSubmittedUsername(username);
-        }}/>
+          }}
+        />
       )}
     </>
   );
---
+++ react-examples/vite/github-finder/src/components/UserSuggestions.tsx
@@ -0,0 +1,29 @@
+import type { GithubUser } from "../types";
+
+export type SuggestionDropdownProps = {
+    suggestions: GithubUser[],
+    show: boolean;
+    onSelect: (username:string) => void
+}
+
+const UserSuggestions = ({suggestions, show, onSelect}:SuggestionDropdownProps) => {
+    if(!show || suggestions.length === 0 ) return null;
+
+    return ( <ul className="suggestions">
+              {suggestions.slice(0, 5).map((user: GithubUser) => (
+                <li
+                  key={user.login}
+                  onClick={() => onSelect(user.login)}
+                >
+                  <img
+                    src={user.avatar_url}
+                    alt={user.login}
+                    className="avatar-xs"
+                  />
+                  {user.login}
+                </li>
+              ))}
+            </ul> );
+}
+ 
+export default UserSuggestions;
