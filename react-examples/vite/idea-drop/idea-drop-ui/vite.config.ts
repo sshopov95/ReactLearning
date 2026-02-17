0c9797d40285ddc5d80fe9fa13f3bee9e328ff80
Светослав Шопов <simobulgaria@gmail.com>
2026-02-17 14:16:36 +0200

    ideadrop project

---
+++ react-examples/vite/idea-drop/idea-drop-ui/vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import { fileURLToPath, URL } from 'node:url'
+import tailwindcss from '@tailwindcss/vite'
+
+const config = defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  plugins: [
+    devtools(),
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+  server:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:8000',
+        changeOrigin: true,
+        rewrite:(path)=> path.replace(/^\/api/,'')
+      }
+    }
+  }
+})
+
+export default config
