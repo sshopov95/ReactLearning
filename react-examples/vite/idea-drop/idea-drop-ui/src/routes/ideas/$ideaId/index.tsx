20353d45d166f1e6072ceb5db5e2f5a39044a9ee
Светослав Шопов <simobulgaria@gmail.com>
2026-02-20 19:35:29 +0200

    Hide controlls

--- react-examples/vite/idea-drop/idea-drop-ui/src/routes/ideas/$ideaId/index.tsx
+++ react-examples/vite/idea-drop/idea-drop-ui/src/routes/ideas/$ideaId/index.tsx
@@ -1,7 +1,11 @@
 import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
-import { queryOptions, useSuspenseQuery,useMutation } from "@tanstack/react-query";
+import {
+  queryOptions,
+  useSuspenseQuery,
+  useMutation,
+} from "@tanstack/react-query";
 import { fetchIdea, deleteIdea } from "@/api/ideas";
-
+import { useAuth } from "@/context/AuthContext";
 
 const ideaQueryOptions = (ideaId: string) =>
   queryOptions({
@@ -20,16 +24,19 @@ function IdeaDetailsPage() {
   const { ideaId } = Route.useParams();
   const { data: idea } = useSuspenseQuery(ideaQueryOptions(ideaId));
   const navigate = useNavigate();
-  const {mutateAsync:deleteMutate, isPending} = useMutation({
-    mutationFn:()=>deleteIdea(ideaId),
-    onSuccess: ()=> {navigate({to:'/ideas'})}
+  const { user } = useAuth();
+  const { mutateAsync: deleteMutate, isPending } = useMutation({
+    mutationFn: () => deleteIdea(ideaId),
+    onSuccess: () => {
+      navigate({ to: "/ideas" });
+    },
   });
   const handleDelete = async () => {
-    const confirmDelete = window.confirm('Are you sure???');
+    const confirmDelete = window.confirm("Are you sure???");
     if (confirmDelete) {
       await deleteMutate();
     }
-  }
+  };
   return (
     <div className="p-4">
       <Link to="/ideas" className="text-blue-500 underline block mb-4">
@@ -37,13 +44,25 @@ function IdeaDetailsPage() {
       </Link>
       <h2 className="text-2xl font-bold">{idea.title}</h2>
       <p className="mt-2">{idea.description}</p>
+      {user && user.id === idea.user && (
+        <>
+          <Link
+            to="/ideas/$ideaId/edit"
+            params={{ ideaId }}
+            className="inline-block text-sm bg-yellow-500 hover:bg-yellow-600 text-white mt-4 mr-2 px-4 py-2 rounded transition"
+          >
+            Edit
+          </Link>
 
-      <Link to="/ideas/$ideaId/edit" params={{ideaId}} className="inline-block text-sm bg-yellow-500 hover:bg-yellow-600 text-white mt-4 mr-2 px-4 py-2 rounded transition">
-      Edit</Link>
-
-      <button disabled={isPending} onClick={handleDelete} className="text-sm bg-red-600 text-white mt-4 mr-2 px-4 py-2 rounded transition hover:bg-red-700 disabled:opacity-50">
-        {isPending? 'Deleting ....':'Delete'}
-      </button>
+          <button
+            disabled={isPending}
+            onClick={handleDelete}
+            className="text-sm bg-red-600 text-white mt-4 mr-2 px-4 py-2 rounded transition hover:bg-red-700 disabled:opacity-50"
+          >
+            {isPending ? "Deleting ...." : "Delete"}
+          </button>
+        </>
+      )}
     </div>
   );
 }
