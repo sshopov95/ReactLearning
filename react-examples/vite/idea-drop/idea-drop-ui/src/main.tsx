d49044d6228db52063317f608c205a1101d9e6be
Светослав Шопов <simobulgaria@gmail.com>
2026-02-20 13:57:06 +0200

    AuthCotnext added

--- react-examples/vite/idea-drop/idea-drop-ui/src/main.tsx
+++ react-examples/vite/idea-drop/idea-drop-ui/src/main.tsx
@@ -1,12 +1,15 @@
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from '@tanstack/react-router'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { getRouter, queryClient } from './router'
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { getRouter, queryClient } from "./router";
+import { AuthProvider } from "./context/AuthContext";
 
-const router = getRouter()
+const router = getRouter();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </AuthProvider>,
+);
